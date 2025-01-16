ae5f50fb03277c8cc3757d0bd8e677897d8cfbb1
Aumetra Weisman <aumetra@cryptolab.net>
2025-01-16 14:56:27 +0100

    Add on-hover play button

--- frontend/src/lib/components/Episode.svelte
+++ frontend/src/lib/components/Episode.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
-	let { cover, title, description }: { cover: string; title: string; description: string } =
-		$props();
+	import Icon from '@iconify/svelte';
+
+	let {
+		cover,
+		title,
+		description,
+		onplay
+	}: { cover: string; title: string; description: string; onplay?: () => void } = $props();
+
+	let hovered = $state(false);
 </script>
 
-<div class="flex w-96 flex-row place-items-center rounded-lg border border-gray-400">
-	<img class="m-0 w-32 rounded-l-lg" src={cover} alt="Cover" />
+<div class="flex w-96 flex-row rounded-lg border border-gray-400">
+	<figure class="relative m-0 w-32 *:h-full *:rounded-l-lg">
+		<img class="w-full" src={cover} alt="Cover" />
 
-	<div class="px-2">
+		<figcaption
+			class="absolute left-0 top-0 m-0 w-full bg-black opacity-0 transition hover:bg-opacity-50 hover:opacity-100"
+		>
+			<button
+				class="h-full w-full"
+				onclick={() => {
+					onplay ? onplay() : {};
+				}}
+			>
+				<Icon width="80" class="m-auto text-white" icon="mdi:play" />
+			</button>
+		</figcaption>
+	</figure>
+
+	<div class="flex flex-col justify-center px-2">
 		<h3 class="m-0 p-0">
 			{title}
 		</h3>
